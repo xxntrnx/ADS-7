9198fc912b22de8eaca440582e7dae1bc1b157c8
ксюша 3 <tyurinaksen111a@gmail.com>
2026-05-31 01:58:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,29 @@
 // Copyright 2022 NNTU-CS
-#include <iostream>
 #include "train.h"
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 int main() {
   srand(time(0));
   for (int n = 2; n <= 50; n += 2) {
-    
     Train train1;
     int count = n;
     while (count--)
       train1.addCar(false);
     train1.getLength();
-    
     Train train2;
     count = n;
     while (count--)
       train2.addCar(true);
     train2.getLength();
-    
     Train train3;
     count = n;
     while (count--)
       train3.addCar(rand() % 2);
     train3.getLength();
-    
-    std::cout << n << "\t" << train1.getOpCount() << "\t\t" 
+    std::cout << n << "\t" << train1.getOpCount() << "\t\t"
               << train2.getOpCount() << "\t\t" << train3.getOpCount() << std::endl;
   }
-  
   return 0;
 }
